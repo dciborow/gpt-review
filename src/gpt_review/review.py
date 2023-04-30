f06322bdcd0bee735dfff199d8f44138ba057469
Daniel Ciborowski <dciborow@microsoft.com>
2023-04-30 18:32:05 -0400

    Update review.py

--- src/gpt_review/review.py
+++ src/gpt_review/review.py
@@ -1,5 +1,8 @@
+from gpt_review._ask import _ask
+
 
 class Reviewer:
+  """Definition of GPT-Review Public Interfaces"""
   def ask(self, prompt: ModelPrompt, params: ModelParams=None, indexes: ModelIndexes=None) -> ModelResponse:
     """Ask a GPT a prompt"""
     _ask(**prompt.asInput(), **params.asInput(), **indexes.asInput())
