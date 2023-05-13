74db07988e525209da57b6f6bad84fad9e2c2c6f
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-13 01:23:20 +0000

    Add support for getting patch and context in Azure DevOps client

--- src/gpt_review/repositories/_devops.py
+++ src/gpt_review/repositories/_devops.py
@@ -2,7 +2,7 @@
 import logging
 import json
 import os
-from typing import Dict, Iterator
+from typing import Dict, Iterator, List, Optional, Iterable
 
 from msrest.authentication import BasicAuthentication
 
@@ -26,6 +26,12 @@ from gpt_review._ask import _ask
 from gpt_review._command import GPTCommandGroup
 from gpt_review._review import _summarize_files
 from gpt_review.repositories._repository import _RepositoryClient
+
+import itertools
+
+
+MIN_CONTEXT_LINES = 5
+SURROUNDING_CONTEXT = 5
 
 
 class _DevOpsClient(_RepositoryClient):
@@ -97,6 +103,7 @@ class _DevOpsClient(_RepositoryClient):
         connection = Connection(base_url=organization_url, creds=credentials)
 
         # Get a client (the "core" client provides access to projects, teams, etc)
+        self.connection = connection
         self.client: GitClient = connection.clients_v7_1.get_git_client()
         self.project = project
         self.repository_id = repository_id
@@ -214,7 +221,7 @@ class _DevOpsClient(_RepositoryClient):
         path: str,
         commit_id: str = None,
         **kwargs,
-    ) -> Iterator[bytes]:
+    ) -> str:
         """
         Read all text from a file.
 
@@ -224,18 +231,33 @@ class _DevOpsClient(_RepositoryClient):
             **kwargs: Any additional keyword arguments.
 
         Returns:
-            Iterator[bytes]: The bytes of the file.
+            str: The text of the file.
         """
-        return self.client.get_item_content(
+        byte_iterator = self.client.get_item_content(
             repository_id=self.repository_id,
             path=path,
             project=self.project,
-            version_descriptor=GitVersionDescriptor(commit_id, version_type="commit"),
+            version_descriptor=GitVersionDescriptor(commit_id, version_type="commit") if commit_id else None,
             **kwargs,
         )
+        return "".join(byte.decode("utf-8") for byte in byte_iterator)
+
+    async def read_all_text_async(self, path: str, commit_id, **kwargs) -> Iterator[bytes]:
+        """
+        Read all text from a file asynchronously.
+
+        Args:
+            path (str): The path to the file.
+            commit_id (str): The commit ID.
+            **kwargs: Any additional keyword arguments.
+
+        Returns:
+            Iterator[bytes]: The bytes of the file.
+        """
+        return await self.client._read_all_text(path=path, commit_id=commit_id)
 
     @staticmethod
-    def process_payload(payload: str):
+    def process_comment_payload(payload: str):
         """
         Extract question from Service Bus payload.
 
@@ -246,8 +268,24 @@ class _DevOpsClient(_RepositoryClient):
             str: The question from the Azure DevOps Comment.
         """
         payload = json.loads(payload)
-        question = payload["resource"]["comment"]["content"]
-        return question
+        return payload["resource"]["comment"]["content"]
+
+    def get_patch(self, pull_request_event, pull_request_id, comment_id) -> List[str]:
+        """
+        Get the diff of a pull request.
+
+        Args:
+            pull_request_event (dict): The pull request event.
+            pull_request_id (str): The Azure DevOps pull request ID.
+            comment_id (str): The Azure DevOps comment ID.
+
+        Returns:
+            List[str]: The diff of the pull request.
+        """
+        context = ContextProvider(self)
+        thread = self._get_comment_thread(pull_request_id=pull_request_id, thread_id=comment_id)
+
+        return context.get_patch(thread_context=thread.thread_context, pull_request_event=pull_request_event)
 
 
 def _review(diff: str = ".diff", link=None, access_token=None) -> Dict[str, str]:
@@ -352,3 +390,187 @@ class DevOpsCommandGroup(GPTCommandGroup):
                 help="The comment ID of Azure DevOps Pull Request Comment.",
                 default=None,
             )
+
+
+class ContextProvider:
+    """Provides context for a given line in a file."""
+
+    def __init__(self, devops_client: _DevOpsClient) -> None:
+        """
+        Initialize a new instance of ContextProvider.
+
+        Args:
+            devops_client (_DevOpsClient): The DevOps client.
+        """
+        self.devops_client = devops_client
+
+    def get_patch(self, thread_context, pull_request_event) -> List[str]:
+        """
+        Get the patch for a given thread context.
+
+        Args:
+            thread_context (ThreadContext): The thread context.
+            pull_request_event (PullRequestEvent): The pull request event.
+
+        Returns:
+            List[str]: The patch.
+        """
+        pull_request = pull_request_event["pullRequest"]
+        if not pull_request:
+            raise ValueError("pull_request_event.pullRequest is required")
+
+        original_content_task = self.devops_client._read_all_text(path=thread_context.file_path, check_if_exists=True)
+        changed_content_task = self.devops_client._read_all_text(
+            path=thread_context.file_path,
+            commit_id=pull_request["lastMergeSourceCommit"]["commitId"],
+            check_if_exists=True,
+        )
+        # original_content = await original_content_task
+        # changed_content = await changed_content_task
+        original_content = original_content_task
+        changed_content = changed_content_task
+
+        left_selection = None
+        right_selection = None
+        if original_content and thread_context.left_file_start and thread_context.left_file_end:
+            left_selection = self._get_selection(
+                original_content, thread_context.left_file_start.line, thread_context.left_file_end.line
+            )
+
+            if not changed_content or not thread_context.right_file_start or not thread_context.right_file_end:
+                raise ValueError("Both left and right selection cannot be None")
+
+            right_selection = self._get_selection(
+                changed_content, thread_context.right_file_start.line, thread_context.right_file_end.line
+            )
+
+        if changed_content and thread_context.right_file_start and thread_context.right_file_end:
+            right_selection = self._get_selection(
+                changed_content, thread_context.right_file_start.line, thread_context.right_file_end.line
+            )
+
+        if not left_selection and not right_selection:
+            raise ValueError("Both left and right selection cannot be None")
+
+        return self._create_patch(left_selection or [], right_selection or [], thread_context.file_path)
+
+    async def get_patches(self, pull_request_event, condensed=False) -> Iterable[List[str]]:
+        """
+        Get the patches for a given pull request event.
+
+        Args:
+            pull_request_event (Any): The pull request event to retrieve patches for.
+            condensed (bool, optional): If True, returns a condensed version of the patch. Defaults to False.
+
+        Returns:
+            Iterable[List[str]]: An iterable of lists containing the patches for the pull request event.
+        """
+        pull_request_id = pull_request_event["pullRequest"]["pullRequestId"]
+        if not pull_request_id:
+            raise ValueError("pull_request_event.pullRequest is required")
+
+        git_changes = await self.devops_client.get_changed_blobs_async(pull_request_event["pullRequest"])
+        all_patches = []
+
+        for git_change in git_changes:
+            all_patches.append(
+                await self._get_change_async(
+                    git_change, pull_request_event["pullRequest"]["lastMergeSourceCommit"]["commitId"], condensed
+                )
+            )
+
+        return all_patches
+
+    def _get_selection(self, file_contents: str, line_start: int, line_end: int) -> List[str]:
+        lines = file_contents.splitlines()
+
+        if line_end - line_start < MIN_CONTEXT_LINES:
+            return lines
+
+        if line_start < 1 or line_start > len(lines) or line_end < 1 or line_end > len(lines):
+            raise ValueError(
+                f"Selection region lineStart = {line_start}, lineEnd = {line_end}, lines length = {len(lines)}"
+            )
+
+        if line_start == line_end:
+            return [lines[line_start - 1]]
+
+        return lines[line_start - 1 : line_end]
+
+    async def _get_change_async(self, git_change, source_commit_head, condensed=False) -> List[str]:
+        return await self._get_git_change_async(self.devops_client, git_change.item.path, source_commit_head, condensed)
+
+    async def _get_git_change_async(self, git_client, file_path, source_commit_head, condensed=False) -> List[str]:
+        original_content = git_client.read_all_text_async(file_path, check_if_exists=True)
+        changed_content = git_client.read_all_text_async(file_path, commit_id=source_commit_head, check_if_exists=True)
+        return self._create_patch(await original_content, await changed_content, file_path, condensed)
+
+    def _create_patch(
+        self, original_content: Optional[str], changed_content: Optional[str], file_path: str, condensed=False
+    ) -> List[str]:
+        left = original_content.splitlines() if original_content else []
+        right = changed_content if changed_content else []
+        return self._create_patch_list(left, right, file_path, condensed)
+
+    def _create_patch_list(self, left: List[str], right: List[str], file_path: str, condensed=False) -> List[str]:
+        dp = self._calculate_minimum_change_needed(left, right)
+        l, r = 1, 1
+        patch = []
+
+        while l < len(left) and r < len(right):
+            if dp[l][r] == dp[l - 1][r - 1]:
+                patch.append(left[l - 1])
+                l += 1
+                r += 1
+            elif dp[l - 1][r] < dp[l][r - 1]:
+                patch.append(f"- {left[l - 1]}")
+                l += 1
+            else:
+                patch.append(f"+ {right[r - 1]}")
+                r += 1
+
+        while l <= len(left):
+            patch.append(f"- {left[l - 1]}")
+            l += 1
+
+        while r <= len(right):
+            patch.append(f"+ {right[r - 1]}")
+            r += 1
+
+        if condensed:
+            patch = self._get_condensed_patch(patch)
+
+        patch.insert(0, file_path)
+        return patch
+
+    def _get_condensed_patch(self, patch: List[str]) -> List[str]:
+        buffer = []
+        result = []
+        trailing_context = 0
+
+        for line in patch:
+            if line.startswith("+") or line.startswith("-"):
+                result.extend(buffer[-SURROUNDING_CONTEXT:])
+                buffer.clear()
+                result.append(line)
+                trailing_context = SURROUNDING_CONTEXT
+            elif trailing_context > 0:
+                result.append(line)
+                trailing_context -= 1
+            else:
+                buffer.append(line)
+
+        return result
+
+    def _calculate_minimum_change_needed(self, left: List[str], right: List[str]) -> List[List[int]]:
+        dp = [[0] * (len(right) + 1) for _ in range(len(left) + 1)]
+
+        for i, j in itertools.product(range(len(left) + 1), range(len(right) + 1)):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif left[i - 1] == right[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+        return dp
--- tests/test_devops.py
+++ tests/test_devops.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import requests_mock
+
 from dataclasses import dataclass
 from azure.devops.v7_1.git.models import (
     GitBaseVersionDescriptor,
@@ -35,6 +36,134 @@ SAMPLE_PAYLOAD = """{
   }
 }
 """
+
+LONG_PAYLOAD = {
+    "id": "e89fa09c-f412-4167-a2cd-f6a5bb8aef56",
+    "eventType": "ms.vss-code.git-pullrequest-comment-event",
+    "publisherId": "tfs",
+    "message": {"text": "Daniel Ciborowski has replied to a pull request comment"},
+    "detailedMessage": {
+        "text": 'Daniel Ciborowski has replied to a pull request comment\r\n```suggestion\n              inlineScript: |                \n                echo "##[section] Summarize Pull Request with Open AI"\n\n                echo "##[command]python3 -m pip install --upgrade pip"\n                python3 -m pip install --upgrade pip --quiet\n```\nhow could i update this code?\r\n'
+    },
+    "resource": {
+        "comment": {
+            "id": 2,
+            "parentCommentId": 1,
+            "author": {
+                "displayName": "Daniel Ciborowski",
+                "url": "https://spsprodwus23.vssps.visualstudio.com/A41b4f3ee-c651-4a14-9847-b7cbb5315b80/_apis/Identities/0ef5b3af-3e01-48fd-9bd3-2f701c8fdebe",
+                "_links": {
+                    "avatar": {
+                        "href": "https://msazure.visualstudio.com/_apis/GraphProfile/MemberAvatars/aad.OTgwYzcxNzEtMDI2Ni03YzVmLTk0YzEtMDNlYzU2YjViYjY4"
+                    }
+                },
+                "id": "0ef5b3af-3e01-48fd-9bd3-2f701c8fdebe",
+                "uniqueName": "dciborow@microsoft.com",
+                "imageUrl": "https://msazure.visualstudio.com/_apis/GraphProfile/MemberAvatars/aad.OTgwYzcxNzEtMDI2Ni03YzVmLTk0YzEtMDNlYzU2YjViYjY4",
+                "descriptor": "aad.OTgwYzcxNzEtMDI2Ni03YzVmLTk0YzEtMDNlYzU2YjViYjY4",
+            },
+            "content": '```suggestion\n              inlineScript: |                \n                echo "##[section] Summarize Pull Request with Open AI"\n\n                echo "##[command]python3 -m pip install --upgrade pip"\n                python3 -m pip install --upgrade pip --quiet\n```\nhow could i update this code?',
+            "publishedDate": "2023-05-13T00:30:56.68Z",
+            "lastUpdatedDate": "2023-05-13T00:30:56.68Z",
+            "lastContentUpdatedDate": "2023-05-13T00:30:56.68Z",
+            "commentType": "text",
+            "usersLiked": [],
+            "_links": {
+                "self": {
+                    "href": "https://msazure.visualstudio.com/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/pullRequests/8063875/threads/141415813/comments/2"
+                },
+                "repository": {
+                    "href": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3"
+                },
+                "threads": {
+                    "href": "https://msazure.visualstudio.com/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/pullRequests/8063875/threads/141415813"
+                },
+                "pullRequests": {"href": "https://msazure.visualstudio.com/_apis/git/pullRequests/8063875"},
+            },
+        },
+        "pullRequest": {
+            "repository": {
+                "id": "612d9367-8ab6-4929-abe6-b5b5ad7b5ad3",
+                "name": "Azure-Gaming",
+                "url": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3",
+                "project": {
+                    "id": "b32aa71e-8ed2-41b2-9d77-5bc261222004",
+                    "name": "One",
+                    "description": "MSAzure/One is the VSTS project containing all Azure team code bases and work items.\nPlease see https://aka.ms/azaccess for work item and source access policies.",
+                    "url": "https://msazure.visualstudio.com/_apis/projects/b32aa71e-8ed2-41b2-9d77-5bc261222004",
+                    "state": "wellFormed",
+                    "revision": 307061,
+                    "visibility": "organization",
+                    "lastUpdateTime": "2023-05-12T17:40:59.963Z",
+                },
+                "size": 508859977,
+                "remoteUrl": "https://msazure.visualstudio.com/DefaultCollection/One/_git/Azure-Gaming",
+                "sshUrl": "msazure@vs-ssh.visualstudio.com:v3/msazure/One/Azure-Gaming",
+                "webUrl": "https://msazure.visualstudio.com/DefaultCollection/One/_git/Azure-Gaming",
+                "isDisabled": False,
+                "isInMaintenance": False,
+            },
+            "pullRequestId": 8063875,
+            "codeReviewId": 8836473,
+            "status": "active",
+            "createdBy": {
+                "displayName": "Daniel Ciborowski",
+                "url": "https://spsprodwus23.vssps.visualstudio.com/A41b4f3ee-c651-4a14-9847-b7cbb5315b80/_apis/Identities/0ef5b3af-3e01-48fd-9bd3-2f701c8fdebe",
+                "_links": {
+                    "avatar": {
+                        "href": "https://msazure.visualstudio.com/_apis/GraphProfile/MemberAvatars/aad.OTgwYzcxNzEtMDI2Ni03YzVmLTk0YzEtMDNlYzU2YjViYjY4"
+                    }
+                },
+                "id": "0ef5b3af-3e01-48fd-9bd3-2f701c8fdebe",
+                "uniqueName": "dciborow@microsoft.com",
+                "imageUrl": "https://msazure.visualstudio.com/_api/_common/identityImage?id=0ef5b3af-3e01-48fd-9bd3-2f701c8fdebe",
+                "descriptor": "aad.OTgwYzcxNzEtMDI2Ni03YzVmLTk0YzEtMDNlYzU2YjViYjY4",
+            },
+            "creationDate": "2023-05-05T03:11:26.8599393Z",
+            "title": "Sample PR Title",
+            "description": "description1",
+            "sourceRefName": "refs/heads/dciborow/update-pr",
+            "targetRefName": "refs/heads/main",
+            "mergeStatus": "succeeded",
+            "isDraft": False,
+            "mergeId": "0e7397c6-5f11-402c-a5c6-c5a12b105350",
+            "lastMergeSourceCommit": {
+                "commitId": "ecea1ea7db038317e94b45e090781410dc519b85",
+                "url": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/commits/ecea1ea7db038317e94b45e090781410dc519b85",
+            },
+            "lastMergeTargetCommit": {
+                "commitId": "36f9a015ee220516f5f553faaa1898ab10972536",
+                "url": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/commits/36f9a015ee220516f5f553faaa1898ab10972536",
+            },
+            "lastMergeCommit": {
+                "commitId": "d5fc735b618647a78a0aff006445b67bfe4e8185",
+                "author": {
+                    "name": "Daniel Ciborowski",
+                    "email": "dciborow@microsoft.com",
+                    "date": "2023-05-05T14:23:49Z",
+                },
+                "committer": {
+                    "name": "Daniel Ciborowski",
+                    "email": "dciborow@microsoft.com",
+                    "date": "2023-05-05T14:23:49Z",
+                },
+                "comment": "Merge pull request 8063875 from dciborow/update-pr into main",
+                "url": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/commits/d5fc735b618647a78a0aff006445b67bfe4e8185",
+            },
+            "reviewers": [],
+            "url": "https://msazure.visualstudio.com/b32aa71e-8ed2-41b2-9d77-5bc261222004/_apis/git/repositories/612d9367-8ab6-4929-abe6-b5b5ad7b5ad3/pullRequests/8063875",
+            "supportsIterations": True,
+            "artifactId": "vstfs:///Git/PullRequestId/b32aa71e-8ed2-41b2-9d77-5bc261222004%2f612d9367-8ab6-4929-abe6-b5b5ad7b5ad3%2f8063875",
+        },
+    },
+    "resourceVersion": "2.0",
+    "resourceContainers": {
+        "collection": {"id": "41bf5486-7392-4b7a-a7e3-a735c767e3b3", "baseUrl": "https://msazure.visualstudio.com/"},
+        "account": {"id": "41b4f3ee-c651-4a14-9847-b7cbb5315b80", "baseUrl": "https://msazure.visualstudio.com/"},
+        "project": {"id": "b32aa71e-8ed2-41b2-9d77-5bc261222004", "baseUrl": "https://msazure.visualstudio.com/"},
+    },
+    "createdDate": "2023-05-13T00:31:02.6421816Z",
+}
 
 
 @pytest.fixture
@@ -134,6 +263,7 @@ def test_update_pr_integration(devops_client: _DevOpsClient) -> None:
 
 @pytest.mark.integration
 def test_get_diff_integration(devops_client: _DevOpsClient) -> None:
+    # response = devops_client.get_diff(SOURCE, TARGET)
     response = devops_client._get_commit_diff(
         diff_common_commit=True,
         base_version=GitBaseVersionDescriptor(version=SOURCE, version_type="commit"),
@@ -143,7 +273,7 @@ def test_get_diff_integration(devops_client: _DevOpsClient) -> None:
 
 
 def process_payload_test() -> None:
-    question = _DevOpsClient.process_payload(SAMPLE_PAYLOAD)
+    question = _DevOpsClient.process_comment_payload(SAMPLE_PAYLOAD)
     link = "https://msazure.visualstudio.com/One/_git/Azure-Gaming/pullrequest/8063875"
     _comment(question, comment_id=COMMENT_ID, link=link)
 
@@ -155,3 +285,11 @@ def test_process_payload(mock_ado_client: None) -> None:
 @pytest.mark.integration
 def test_process_payload_integration() -> None:
     process_payload_test()
+
+
+def test_get_patch(devops_client: _DevOpsClient) -> None:
+    comment_id = LONG_PAYLOAD["resource"]["comment"]["_links"]["threads"]["href"].split("/")[-1]
+    patch = devops_client.get_patch(
+        pull_request_event=LONG_PAYLOAD["resource"], pull_request_id=PR_ID, comment_id=comment_id
+    )
+    assert len(patch) == 64
